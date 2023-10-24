b612ea4ac31b520ca19cd10161815ae177ec6d74
qtai2901 <67884469+qtai2901@users.noreply.github.com>
2023-10-24 17:05:11 +0700

    Update model.go

--- api/v2board/model.go
+++ api/v2board/model.go
@@ -4,4 +4,16 @@ type UserTraffic struct {
 	UID   int `json:"user_id"`
 	Upload   int64  `json:"u"`
 	Download int64  `json:"d"`
+	Ip       string `json:"ip"`
+}
+type OnlineUser struct {
+	UID int    `json:"user_id"`
+	IP  string `json:"ip"`
+}
+// UserResponse is the response of user
+type UserResponse struct {
+	ID          int     `json:"id"`
+	Passwd      string  `json:"passwd"`
+	SpeedLimit  float64 `json:"nodeSpeedlimit"`
+	DeviceLimit int     `json:"nodeConnector"`
 }
